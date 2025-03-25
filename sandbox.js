95457a07f37c3d6e53ae72b9f607d5184a025fda
Yutthana Tantinimitchok <tan@inspectrealestate.com.au>
2025-03-25 21:17:38 +1000

    code for lesson 2 about type

--- sandbox.js
+++ sandbox.js
@@ -1,7 +1,14 @@
+// const character = 'Tanaka';
+// console.log(character);
+// const inputs = document.querySelectorAll('input');
+// console.log(inputs);
+// inputs.forEach(input => {
+//   console.log(input);   });
+/* Lesson 2 */
 var character = 'Tanaka';
-console.log(character);
-var inputs = document.querySelectorAll('input');
-console.log(inputs);
-inputs.forEach(function (input) {
-    console.log(input);
-});
+var age = 30;
+var isBlackBelt = false;
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circ(40));
--- sandbox.ts
+++ sandbox.ts
@@ -1,8 +1,19 @@
-const character = 'Tanaka';
-console.log(character);
+// const character = 'Tanaka';
+// console.log(character);
 
-const inputs = document.querySelectorAll('input');
-console.log(inputs);
+// const inputs = document.querySelectorAll('input');
+// console.log(inputs);
 
-inputs.forEach(input => {
-  console.log(input);   });
+// inputs.forEach(input => {
+//   console.log(input);   });
+
+/* Lesson 2 */
+let character = 'Tanaka';
+let age = 30;
+let isBlackBelt = false;
+
+const circ = (diameter: number) => {
+  return diameter * Math.PI;
+}
+
+console.log(circ(40));
