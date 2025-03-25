ba6ed88d8388dd355ee450f830eacb61d9ee9f9c
Yutthana Tantinimitchok <tan@inspectrealestate.com.au>
2025-03-25 21:29:18 +1000

    code lesson 3 array and object

--- sandbox.js
+++ sandbox.js
@@ -5,10 +5,34 @@
 // inputs.forEach(input => {
 //   console.log(input);   });
 /* Lesson 2 */
-var character = 'Tanaka';
-var age = 30;
-var isBlackBelt = false;
-var circ = function (diameter) {
-    return diameter * Math.PI;
+// let character = 'Tanaka';
+// let age = 30;
+// let isBlackBelt = false;
+// const circ = (diameter: number) => {
+//   return diameter * Math.PI;
+// }
+// console.log(circ(40));
+/* Lesson 3 */
+// Arrays   
+var names = ['Tanaka', 'Suzuki', 'Yamada'];
+names.push('Hiroshi');
+// names.push(3); // Error
+var numbers = [10, 20, 30, 40];
+numbers.push(25); // OK       
+// numbers.push('Shaun'); // Error
+numbers[1] = 100; // OK
+var mixed = ['Ken', 4, 'Taro', 8, 9]; // OK array can have different types need to be defined in the beginning 
+// but should be avoided
+//objects
+var ninja = {
+    name: 'Tanaka',
+    belt: 'black',
+    age: 30
 };
-console.log(circ(40));
+ninja.age = 40; // OK
+// ninja.age = 'string'; // Error
+ninja = {
+    name: 'Yamada',
+    belt: 'orange',
+    age: 20
+};
--- sandbox.ts
+++ sandbox.ts
@@ -8,12 +8,45 @@
 //   console.log(input);   });
 
 /* Lesson 2 */
-let character = 'Tanaka';
-let age = 30;
-let isBlackBelt = false;
+// let character = 'Tanaka';
+// let age = 30;
+// let isBlackBelt = false;
 
-const circ = (diameter: number) => {
-  return diameter * Math.PI;
+// const circ = (diameter: number) => {
+//   return diameter * Math.PI;
+// }
+
+// console.log(circ(40));
+
+/* Lesson 3 */
+// Arrays   
+let names = ['Tanaka', 'Suzuki', 'Yamada'];
+
+names.push('Hiroshi');
+// names.push(3); // Error
+
+let numbers = [10, 20, 30, 40];
+numbers.push(25);   // OK       
+// numbers.push('Shaun'); // Error
+numbers[1] = 100; // OK
+
+
+let mixed = ['Ken', 4, 'Taro', 8, 9]; // OK array can have different types need to be defined in the beginning 
+// but should be avoided
+
+
+//objects
+
+let ninja = {
+  name: 'Tanaka',
+  belt: 'black',
+  age: 30
+};
+
+ninja.age = 40; // OK
+// ninja.age = 'string'; // Error
+ninja = {
+  name: 'Yamada',
+  belt: 'orange',
+  age: 20
 }
-
-console.log(circ(40));
